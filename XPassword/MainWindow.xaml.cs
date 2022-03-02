66458de625999a9139f2437fad0df7fccdb1ad1b
Timur Kulkaev <kulkaevta@mail.ru>
2022-03-02 12:47:25 +0300

    Фикс

--- XPassword/MainWindow.xaml.cs
+++ XPassword/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace XPassword
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            TextBoxOutputGeneratedPassword.Text = "";
         }
     }
 }
