8345175e7e04ae9733be6ea20e2166e867529698
Timur Kulkaev <kulkaevta@mail.ru>
2022-03-24 22:50:48 +0300

    edit

--- XPassword/WindowsXpassword.xaml.cs
+++ XPassword/WindowsXpassword.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,16 @@ using System.Windows.Shapes;
 
 namespace XPassword
 {
-    /// <summary>
-    /// Логика взаимодействия для WindowsXpassword.xaml
-    /// </summary>
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } // модель телефона
+        public string Company { get; set; } // производитель
+        public string ImagePath { get; set; } // путь к изображению
+    }
+
+
+
     public partial class WindowsXpassword : Window
     {
         bool leftMenuShowed = true;
@@ -24,6 +32,14 @@ namespace XPassword
         public WindowsXpassword()
         {
             InitializeComponent();
+            var Phones = new ObservableCollection<Phone>
+        {
+            new Phone {Id=1, ImagePath="/Images/iphone6s.jpg", Title="iPhone 6S", Company="Apple" },
+            new Phone {Id=2, ImagePath="/Images/lumia950.jpg", Title="Lumia 950", Company="Microsoft" },
+            new Phone {Id=3, ImagePath="/Images/nexus5x.jpg", Title="Nexus 5X", Company="Google" },
+            new Phone {Id=4, ImagePath="/Images/galaxys6.jpg", Title="Galaxy S6", Company="Samsung"}
+        };
+            phonesList.ItemsSource = Phones;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
